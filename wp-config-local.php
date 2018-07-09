1b99449c68657b60df2d01935f797716666d0635
Leticia Matos <leticia@Comp-de-Leticia-154.local>
2018-07-09 15:36:51 -0300

    wp-config

--- wp-config-local.php
+++ wp-config-local.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'we7emeau6u4mc2am');
+define('DB_NAME', 'wp_w2g');
 
 /** MySQL database username */
-define('DB_USER', 'c6lnsu3p979e2ydp');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'xjgzl9swbcront03');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'c9cujduvu830eexs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
